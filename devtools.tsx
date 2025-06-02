0771772ca7bd8e6c92d2d9903451027b741f73be
Christoph Ebert <christoph.ebert@digitecgalaxus.ch>
2025-06-02 17:30:46 +0200

    Remove some unnecessary stuff

--- devtools.tsx
+++ devtools.tsx
@@ -1,5 +1,4 @@
 import performanceInsightsHTML from "url:./panels/performance-insights/index.html";
-import fontPropertiesHTML from "url:./panels/font-properties/index.html";
 
 chrome.devtools.panels.create(
   "DG Performance Insights",
@@ -7,20 +6,3 @@ chrome.devtools.panels.create(
   // See: https://github.com/PlasmoHQ/plasmo/issues/106#issuecomment-1188539625
   performanceInsightsHTML.split("/").pop()
 );
-
-chrome.devtools.panels.elements.createSidebarPane(
-  "Font Properties",
-  function (sidebar) {
-    sidebar.setPage(fontPropertiesHTML.split("/").pop());
-  }
-);
-
-function IndexDevtools() {
-  return (
-    <h2>
-      Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-    </h2>
-  );
-}
-
-export default IndexDevtools;
