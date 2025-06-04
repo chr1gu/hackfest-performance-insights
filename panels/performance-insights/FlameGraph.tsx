e50944c6d2bc1bbf16cfc01cf110fba4d99b062f
Christian Sany <christian.sany@digitecgalaxus.ch>
2025-06-04 15:21:58 +0200

    Add more flame graph stuff

--- panels/performance-insights/FlameGraph.tsx
+++ panels/performance-insights/FlameGraph.tsx
@@ -2,7 +2,10 @@ import type {
   CSSProperties,
   FunctionComponent,
 } from "~node_modules/@types/react";
-import type { CompletePageInsightRequest } from "~shared/pageInsights";
+import {
+  GraphQlGatewayHostSystem,
+  type CompletePageInsightRequest,
+} from "~shared/pageInsights";
 
 interface FlameGraphProps {
   request: CompletePageInsightRequest;
@@ -11,30 +14,65 @@ interface FlameGraphProps {
 export const FlameGraph: FunctionComponent<FlameGraphProps> = ({ request }) => {
   const totalDuration = request.response.totalDuration;
 
+  console.dir(request.response);
+
   return (
     <table style={{ width: "100%", borderCollapse: "collapse" }}>
       <tbody>
         <tr>
           <th style={thStyles}>Akamai</th>
           <td style={tdStyles}>
-            <div>
-              <TimeSpan
-                duration={
-                  (request.response.akamaiInfo.edgeDuration / totalDuration) *
-                  100
-                }
-                offset={0}
-              />
-              <TimeSpan
-                duration={
-                  (request.response.akamaiInfo.originDuration / totalDuration) *
-                  100
-                }
-                offset={0}
-              />
-            </div>
+            <TimeSpan
+              duration={request.response.akamaiInfo.edgeDuration}
+              durationInPercent={
+                (request.response.akamaiInfo.edgeDuration / totalDuration) * 100
+              }
+              offset={0}
+            />
+            <TimeSpan
+              duration={request.response.akamaiInfo.originDuration}
+              durationInPercent={
+                (request.response.akamaiInfo.originDuration / totalDuration) *
+                100
+              }
+              offset={0}
+            />
           </td>
         </tr>
+        {request.response.hosts.map((host, index) => (
+          <>
+            <tr key={index}>
+              <th style={thStyles}>{host.name}</th>
+              <td style={tdStyles}>
+                <TimeSpan
+                  duration={host.duration || 0}
+                  durationInPercent={
+                    ((host.duration || 0) / totalDuration) * 100
+                  }
+                  offset={0}
+                />
+              </td>
+            </tr>
+
+            {host instanceof GraphQlGatewayHostSystem
+              ? host.subGraphQueries?.map((subQuery, subIndex) => (
+                  <tr key={subIndex}>
+                    <th style={thStyles}>{host.name}</th>
+                    <td style={tdStyles}>
+                      <TimeSpan
+                        key={subIndex}
+                        duration={subQuery.duration || 0}
+                        durationInPercent={
+                          ((subQuery.duration || 0) / totalDuration) * 100
+                        }
+                        offset={0}
+                      />
+                    </td>
+                  </tr>
+                ))
+              : null}
+          </>
+        ))}
       </tbody>
     </table>
   );
@@ -42,23 +80,36 @@ export const FlameGraph: FunctionComponent<FlameGraphProps> = ({ request }) => {
 
 interface TimeSpanProps {
   duration: number;
+  durationInPercent: number;
   offset: number;
 }
 
-const TimeSpan: FunctionComponent<TimeSpanProps> = ({ duration, offset }) => {
+const TimeSpan: FunctionComponent<TimeSpanProps> = ({
+  duration: time,
+  durationInPercent: duration,
+  offset,
+}) => {
   return (
     <span
       style={{
         display: "block",
-        marginLeft: `calc(${offset}% + 1px)`,
-        marginRight: "1px",
+        marginLeft: `calc(${offset}% + 2px)`,
+        marginRight: "2px",
         width: `calc(${duration}% - 4px)`,
-        height: "20px",
+        height: "24px",
         backgroundColor: "#228be6",
         float: "left",
-        border: "1px solid #000",
+        fontSize: "14px",
+        color: "#fff",
+        textAlign: "center",
+        lineHeight: "24px",
+        borderRadius: "2px",
+        letterSpacing: "0.01em",
       }}
-    ></span>
+      title={`${time}ms`}
+    >
+      {time}ms
+    </span>
   );
 };
 
@@ -74,9 +125,6 @@ const thStyles: CSSProperties = {
 
 const tdStyles: CSSProperties = {
   position: "relative",
-  textAlign: "left",
-  fontSize: "14px",
   padding: "4px 0",
   borderBottom: "1px solid rgba(221, 221, 221, .5)",
-  letterSpacing: "0.01em",
 };
