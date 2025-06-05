291502b5ffea78d5a12b2a4917e8c262af583fd9
Roman <roman.huerlimann@digitecgalaxus.ch>
2025-06-05 09:33:56 +0200

    add grapholithHandler.ts

---
+++ requestHandlers/grapholithHandler.ts
@@ -0,0 +1,96 @@
+import {
+  GrapholithHostSystem,
+  GraphQlGatewayHostSystem,
+  SubGraphQuery,
+  type PageInsightRequest,
+} from "~shared/pageInsights";
+import {
+  findServerTimingHeader,
+  getAkamaiInfo,
+  type RequestHandler,
+} from "./requestHandler";
+import { getPageInsights, updatePageInsights } from "~shared/storage";
+
+// 'server-timing', value: 'dg-trace-gql-grapholith;description=get-brand-like;dur=7'}
+// 'server-timing', value: 'dg-trace-gql-grapholith;description=get-brand-initialprops;dur=17'}
+export function getGrapholithGatewaySystems(
+  request: chrome.webRequest.WebResponseHeadersDetails
+): GrapholithHostSystem[] {
+  const hostSystems: GrapholithHostSystem[] = [];
+  request.responseHeaders
+    ?.filter(
+      (header) =>
+        header.name.toLowerCase() === "server-timing" &&
+        header.value?.startsWith("dg-trace-gql-grapholith")
+    )
+    .forEach((header) => {
+      const gatewayParts = header.value?.split(";") || [];
+      const gatewayQueryName =
+        gatewayParts
+          .find((part) => part.trim().startsWith("description="))
+          ?.replace("description=", "")
+          .trim() ?? "Unknown layout query";
+
+      const gatewayDuration = parseInt(
+        gatewayParts
+          .find((part) => part.trim().startsWith("dur="))
+          ?.replace("dur=", "") || "0"
+      );
+
+      const gatewayHost = new GrapholithHostSystem();
+      gatewayHost.duration = gatewayDuration;
+      gatewayHost.queryName = gatewayQueryName;
+
+      hostSystems.push(gatewayHost);
+    });
+
+  return hostSystems;
+}
+
+export class GrapholithHandler implements RequestHandler {
+  canHandleRequest(request: chrome.webRequest.WebRequestDetails): boolean {
+    return (
+      request.type === "xmlhttprequest" &&
+      request.url.includes("api/graphql/") &&
+      request.method === "POST"
+    );
+  }
+
+  onBeforeSendHeaders(request: chrome.webRequest.WebRequestDetails): void {
+    getPageInsights((pageInsights) => {
+      const requestInfo: PageInsightRequest = {
+        name: request.url.split("/").pop() || "Grapholith Request",
+        type: "Grapholith",
+        requestId: request.requestId,
+        completed: false,
+      };
+
+      pageInsights.requests.push(requestInfo);
+      updatePageInsights(pageInsights);
+    });
+  }
+
+  onCompleted(request: chrome.webRequest.WebResponseHeadersDetails): void {
+    getPageInsights((pageInsights) => {
+      const requestInfo = pageInsights.requests.find(
+        (req) => req.requestId === request.requestId
+      );
+
+      if (requestInfo) {
+        const akamaiInfo = getAkamaiInfo(request);
+
+        requestInfo.completed = true;
+        // This if statement is simply here as a lazy way to fix ts error
+        if (requestInfo.completed) {
+          requestInfo.response = {
+            totalDuration: akamaiInfo.edgeDuration + akamaiInfo.originDuration,
+            akamaiInfo,
+            hosts: getGrapholithGatewaySystems(request), // This can be populated with more detailed host information if needed
+          };
+        }
+
+        updatePageInsights(pageInsights);
+      }
+    });
+  }
+}
