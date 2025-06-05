eab08a1c0a747d27f569871927e025258b3bc8d1
Christian Sany <christian.sany@digitecgalaxus.ch>
2025-06-05 11:15:40 +0200

    Fix flamge graph colors for grapholith

--- panels/performance-insights/InfrastructureTag.tsx
+++ panels/performance-insights/InfrastructureTag.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent } from "~node_modules/@types/react";
+import { colorMap } from "./colors";
 
 type InfrastructureTagProps = {
   count: number;
@@ -9,42 +10,6 @@ type InfrastructureTagProps = {
     | "Grapholith"
     | "Subgraph"
     | "Database";
-};
-
-const colorMap: Record<
-  InfrastructureTagProps["tag"],
-  { borderColor: string; color: string; backgroundColor: string }
-> = {
-  Akamai: {
-    color: "#fff",
-    backgroundColor: "#228be6",
-    borderColor: "#228be6",
-  },
-  Frontend: {
-    color: "#fff",
-    backgroundColor: "#40c057",
-    borderColor: "#40c057",
-  },
-  Gateway: {
-    color: "#fff",
-    backgroundColor: "#fa5252",
-    borderColor: "#fa5252",
-  },
-  Grapholith: {
-    color: "#fff",
-    backgroundColor: "#fa5252",
-    borderColor: "#fa5252",
-  },
-  Subgraph: {
-    color: "#fff",
-    backgroundColor: "#fa5252",
-    borderColor: "#fa5252",
-  },
-  Database: {
-    color: "#000",
-    backgroundColor: "#eeee00",
-    borderColor: "#eeee00",
-  },
 };
 
 export const InfrastructureTag: FunctionComponent<InfrastructureTagProps> = ({
